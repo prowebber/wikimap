195127249016b565f20716e1c643f6c05637b370
Steven Holdaway <webpurellc@gmail.com>
2017-10-16 07:01:43 -0400

    Added a connection test.

---
+++ public/js/server-bridge.js
@@ -0,0 +1,23 @@
+function ajaxFetch(form_data){
+	return $.ajax({
+		method: "POST",
+		data: form_data,
+		url: 'http://src.purewebber.com/src/scripts/wikimaps.php',
+		dataType: json
+	});
+}
+
+
+function databaseRequest(t0_page_id){
+	var form_data = [];
+	form_data.push({name: 't0_page_id', value: t0_page_id});
+	form_data.push({name: 'server_class', value: 'fetchT0Data'});
+
+	ajaxFetch(form_data).done(function (data) {			// Call the Ajax function and wait for it to finish
+		console.log(data);
+	});
+}
+
+$(function() {
+	databaseRequest('54484061');		// Sugar bee
+}
