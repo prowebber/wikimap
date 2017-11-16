f4039d537da2111043ea85601ed4168cd6c63d2b
ch4ot1k <charlesrlemonds@gmail.com>
2017-11-15 21:07:54 -0500

    made things work again

--- src/js/server-bridge.js
+++ src/js/server-bridge.js
@@ -44,14 +44,14 @@ function databaseRequest(user_input){
 
 	});
 }
+// var MIN_SHARED_LINKS = min_shared_links;
+// // console.log('Min shared links: ' +  MIN_SHARED_LINKS);
+// var MAX_SHARED_LINKS = max_shared_links;
+// // console.log('Max shared links: ' +  MAX_SHARED_LINKS);
+// var STRENGTH_SCALE = 0.4;
 function showGraph(json_response){
 	const CAMERA_DISTANCE2NODES_FACTOR = 10;
 	const Graph = ForceGraph3D()
-	const MIN_SHARED_LINKS = min_shared_links;
-	console.log('Min shared links: ' +  MIN_SHARED_LINKS);
-	const MAX_SHARED_LINKS = max_shared_links;
-	console.log('Max shared links: ' +  MAX_SHARED_LINKS);
-	const STRENGTH_SCALE = 0.4;
 	(document.getElementById('3d-graph'))
 		.graphData(json_response)
 		.onNodeClick(colorNode);
