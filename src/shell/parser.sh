2aa7cd74e442b659eb594844a58e3101693b7bb8
ch4ot1k <charlesrlemonds@gmail.com>
2017-11-29 07:24:33 -0500

    fastest parser.sh yet, needs beginning and ending text removed (insert into and shizz)

--- src/shell/parser.sh
+++ src/shell/parser.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-input_file="C:/Users/Charles/PycharmProjects/untitled/all_page_links.sql"
-output_file="C:/Users/Charles/PycharmProjects/untitled/shell_output_2.txt"
- time sed "s/ //g" $input_file >> $output_file
+input_file="C:/Users/Charles/PycharmProjects/untitled/pagelinks_small_4.sql"
+output_file_1="C:/Users/Charles/PycharmProjects/untitled/shell_output_1.txt"
+output_file_2="C:/Users/Charles/PycharmProjects/untitled/shell_output_2.txt"
+output_file_3="C:/Users/Charles/PycharmProjects/untitled/shell_output_3.txt"
+output_file_4="C:/Users/Charles/PycharmProjects/untitled/shell_output_4.txt"
+
+time sed s/\)\,\(/\\n/g $input_file > $output_file_1		#parse by ),(
+time sed /\,0\,.*\,0/!d $output_file_1 > $output_file_2
+time sed s/\,0\,\'/\\t/ $output_file_2 > $output_file_3
+time sed s/\'\,0/\\t/ $output_file_3 > $output_file_4
