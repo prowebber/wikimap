86ced59193bc5cea5bbd808f6ed18a1041174ba6
ch4ot1k <charlesrlemonds@gmail.com>
2017-11-30 12:43:12 -0500

    parser updated to faster one

--- src/shell/parser.sh
+++ src/shell/parser.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-input_file="C:/Users/Charles/PycharmProjects/untitled/pagelinks_small_4.sql"
-output_file_1="C:/Users/Charles/PycharmProjects/untitled/shell_output_1.txt"
-output_file_2="C:/Users/Charles/PycharmProjects/untitled/shell_output_2.txt"
-output_file_3="C:/Users/Charles/PycharmProjects/untitled/shell_output_3.txt"
-output_file_4="C:/Users/Charles/PycharmProjects/untitled/shell_output_4.txt"
-
-time sed s/\)\,\(/\\n/g $input_file > $output_file_1		#parse by ),(
-time sed /\,0\,.*\,0/!d $output_file_1 > $output_file_2
-time sed s/\,0\,\'/\\t/ $output_file_2 > $output_file_3
-time sed s/\'\,0/\\t/ $output_file_3 > $output_file_4
+input_file="C:/Users/Charles/PycharmProjects/untitled/all_page_links.sql"
+output_file="C:/Users/Charles/PycharmProjects/untitled/shell_output.txt"
+# Remove prefix ; remove last character ; replace "),(" with newline '\n' ; replace any string within ',0,
+time LC_ALL=C sed -n "s/INSERT INTO \`pagelinks\` VALUES (// ; s/.$// ; s/),(/\\n/g ; s/\\n[^\\n]*,0,'[^']*',0[^0]*\\n/\\n/g ; s/,0,'/\\t/g ; s/',0/\\t/gp" $input_file > $output_file
+#; s/,0,'/\\t/g ; s/',0/\\t/gp
+#time sed -n "s/\\n.*,0,.*,0\\n/\\n/p" $output_file > $output_file_2
+#time sed -n "s/,0,'/\\t/ ; s/',0/\\t/" $output_file_2 > $output_file_3
+#time sed "s/',0/\\t/" $output_file_3 > $output_file_4
--- src/shell/parser_new.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-input_file="C:/Users/Charles/PycharmProjects/untitled/all_page_links.sql"
-output_file="C:/Users/Charles/PycharmProjects/untitled/shell_output.txt"
-# Remove prefix ; remove last character ; replace "),(" with newline '\n' ; replace any string within ',0,
-time LC_ALL=C sed -n "s/INSERT INTO \`pagelinks\` VALUES (// ; s/.$// ; s/),(/\\n/g ; s/\\n[^\\n]*,0,'[^']*',0[^0]*\\n/\\n/g ; s/,0,'/\\t/g ; s/',0/\\t/gp" $input_file > $output_file
-#; s/,0,'/\\t/g ; s/',0/\\t/gp
-#time sed -n "s/\\n.*,0,.*,0\\n/\\n/p" $output_file > $output_file_2
-#time sed -n "s/,0,'/\\t/ ; s/',0/\\t/" $output_file_2 > $output_file_3
-#time sed "s/',0/\\t/" $output_file_3 > $output_file_4
