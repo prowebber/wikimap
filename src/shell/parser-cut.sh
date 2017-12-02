8ed1463712a5801a7a46eda1f4f8c0bd48146209
ch4ot1k <charlesrlemonds@gmail.com>
2017-12-02 18:19:27 -0500

    parser optimized to 19 MB/s, vars added for readibility

--- src/shell/parser-cut.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-input_file="C:/Users/Charles/PycharmProjects/untitled/all_page_links.sql"
-output_file="C:/Users/Charles/PycharmProjects/untitled/shell_output.txt"
-# time, locale to C, Remove prefix ; remove last character ; replace "),(" with newline '\n' ; replace any string within ',0,
-time LC_ALL=C sed -n "s/INSERT INTO \`pagelinks\` VALUES (// ; s/.$// ; s/),(/\\n/g ; s/\\n[^\\n]*,0,'[^']*',0[^0]*\\n/\\n/g ; s/,0,'/\\t/g ; s/',0/\\t/gp" $input_file > $output_file
--- src/shell/parser.sh
+++ src/shell/parser.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-input_file="C:/Users/Charles/PycharmProjects/untitled/all_page_links.sql"
+input_file="C:/Users/Charles/PycharmProjects/untitled/pagelinks_small_4.sql"
 output_file="C:/Users/Charles/PycharmProjects/untitled/shell_output.txt"
-# time, locale to C, Remove prefix ; remove last character ; replace "),(" with newline '\n' ; replace any string within ',0,
-time LC_ALL=C sed -n "s/INSERT INTO \`pagelinks\` VALUES (// ; s/.$// ; s/),(/\\n/g ; s/\\n[^\\n]*,0,'[^']*',0[^0]*\\n/\\n/g ; s/,0,'/\\t/g ; s/',0/\\t/gp" $input_file > $output_file
+# String substitutions
+remove_prefix="s/INSERT INTO \`pagelinks\` VALUES (//"
+remove_suffix="s/);//"
+parse_newline="s/),(/\\n/g"
+omit_nonzero_namespace1="s/\\n[^\\n]*,[^0]*,*[^\\n]*\\n/\\n/g"
+omit_nonzero_namespace2="s/\\n[^\\n]*',[^0]\\n/\\n/g"
+parse_tab1="s/,0,'/\\t/g"
+parse_tab2="s/',0/\\t/gp"
+# Parse that shit
+time LC_ALL=C sed -n "$remove_prefix;$remove_suffix;$parse_newline;$omit_nonzero_namespace1;$omit_nonzero_namespace2;$parse_tab1;$parse_tab2" $input_file > $output_file
