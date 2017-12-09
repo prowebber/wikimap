2775874bf0b4199cfd3050f931d93898a116c8de
ch4ot1k <charlesrlemonds@gmail.com>
2017-12-09 07:38:42 -0500

    added first 720MB (requires git LFS 'git install lfs' then 'git lfs track '*.sql'' then add as normal

--- src/shell/parser-page-leftovers.sh
+++ src/shell/parser-page-leftovers.sh
@@ -6,6 +6,6 @@ remove_prefix="s/INSERT INTO \`page\` VALUES (//"
 remove_suffix="s/);//"
 parse_newline="s/),(/\\n/g"
 remove_extra_shit="s/','*[^\\n]*[\\n$]/\\n/g"
-omit_nonzero_namespace="s/\\n[^\\n]*,[1-9],\\W.*//gp"
+omit_nonzero_namespace="s/\\n[^\\n]*,[1-9],*//gp"
 # Parse that shit
 time LC_ALL=C sed -n "$remove_prefix;$remove_suffix;$parse_newline;$remove_extra_shit;$omit_nonzero_namespace" $input_file > $output_file
