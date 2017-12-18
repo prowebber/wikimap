326ecd963b763c593abaa46e944c3daa23d2ed0d
ch4ot1k <charlesrlemonds@gmail.com>
2017-12-18 08:57:54 -0500

    added pagelinks_compiler with different ways of flipping page_array

--- src/scripts/pagelinks_compiler.php
+++ src/scripts/pagelinks_compiler.php
@@ -5,9 +5,10 @@ ini_set('memory_limit', '10048M');                              # 10 GB memory l
 $output_file     = "../../pagelinks_final_TSV.txt";
 $start_time     = microtime(TRUE);
 $page_array     = page_array_no_redirects(redirect_array());
-$pagelinks_final_string = implode("\n", compiled_pagelinks($page_array));
+$pagelinks_final = compiled_pagelinks($page_array);
+$pagelinks_string = implode("\n",$pagelinks_final);
 #3,027,571 rows
-file_put_contents($output_file, $pagelinks_final_string);
+file_put_contents($output_file, $pagelinks_string);
 $end_time = number_format((microtime(TRUE) - $start_time), 6); # Calculate how long it took
 echo "Total Time: $end_time (sec.)";                                          # Display the run time results
 
@@ -38,7 +39,14 @@ function page_array_no_redirects($redirect_array){
 	return $page_array;
 }
 function compiled_pagelinks($page_array){
-	$flipped_page_array = array_flip($page_array);
+	echo "Page array size before flip:".count($page_array)."\n";
+//	$flipped_page_array = array_flip($page_array);
+//	foreach($page_array as $key => $value){
+//		$flipped_page_array[]=array($value, $key);
+//	}
+	$flipped_page_array=array_combine(array_values($page_array),array_keys($page_array));
+	file_put_contents("../../flipped_page_array_new.txt", print_r($flipped_page_array, TRUE));
+	echo "Flipped page array size after flip:".count($flipped_page_array)."\n";
 	$count = 0;
 	$contents = file_get_contents("../../pagelinks_parsed.txt", TRUE);   # Convert the file to a string
 	$tok = strtok($contents, "\n");                                             # Split the string by token (new line)
@@ -53,4 +61,8 @@ function compiled_pagelinks($page_array){
 	}
 	echo 'Count: '.$count."\n";
 	return $pagelinks_final;
+}
+function pagelinks_reversed_appended($page_links){
+	$flipped_page_array=array_flip($page_links);
+	return $page_links + $flipped_page_array;
 }
---
+++ src/scripts/shared_links_compiler.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace datapeak\public_html\src\scripts;
+
+use datapeak\server\classes\SQL_Database;
+
+include 'C:/Users/steve/Dropbox/servers/client_server/data/datapeak/public_html/config.php';
+#include $_SERVER['DOCUMENT_ROOT'] . "/config.php";
+
+set_time_limit(3600);                                                # 60 minute running limit
+ini_set('memory_limit', '10048M');                           # 10 GB memory limit
+
+
+class Run_Loop{
+	public function __construct(){
+		#$this->db = new SQL_Database(WIKIMAP_DB);                           # Initiate database testing
+		$this->db = new SQL_Database(LOCAL_DB);                            # Used only for local testing
+	}
+	
+	
+	
+	/**
+	 * Start Here
+	 */
+	public function classConfig(){
+		$sample_set = $this->getSampleSetLocal();
+		#$sample_set = $this->getSampleSet();
+		$this->findSimilarities($sample_set);
+	}
+	
+	
+	
+	/**
+	 * Used only for local testing
+	 */
+	public function getSampleSetLocal(){
+		$page_id_array   = array();         # Array to store the page IDs for the sample set
+		$page_id_array[] = '18978754';      # Apple
+		$page_id_array[] = '44752456';      # Apple Crab
+		$page_id_array[] = '159187';        # Apple Pie
+		
+		// Convert the page_id_array into a comma-separated string for MySQL
+		$sql_string = implode(',', $page_id_array);
+		$i = 0;
+		$result = $this->db->query("	SELECT
+											pc.T0,
+											pc.T1
+										FROM wiki_links.page_connections pc
+										WHERE
+											pc.T0 <= '10000'
+											AND pc.T0 != pc.T1
+                                ");
+		
+		while($row = $result->fetch_assoc()){
+			$T0_page_id = $row['T0'];
+			$T1_page_id = $row['T1'];
+			$data[$T0_page_id][$T1_page_id] = $T1_page_id;
+			$i++;
+		}
+		echo "<b>Total Rows:</b> ".number_format($i)." <br>";
+		return $data;
+	}
+	
+	public function getSampleSet(){
+		$i = 0;
+		$result = $this->db->query("	SELECT
+											pc.T0,
+											pc.T1
+										FROM wikimap.page_connections pc
+										WHERE
+											pc.T0 <= '500'
+											AND pc.T0 != pc.T1
+                                ");
+		while($row = $result->fetch_assoc()){
+			$T0_page_id = $row['T0'];
+			$T1_page_id = $row['T1'];
+			$data[$T0_page_id][$T1_page_id] = $T1_page_id;
+			$i++;
+		}
+		echo "<b>Total Rows:</b> ".number_format($i)." <br>";
+		return $data;
+	}
+	
+	public function findSimilarities($sample_set){
+		$final = array();
+		$t0_array = array_keys($sample_set);                # Create an array of all the T0's
+		$start_time = microtime(true);
+		foreach($t0_array as $t0){                          # Loop through each T0
+			foreach($sample_set[$t0] as $t1){               # Loop through each T0->T1
+				if(!isset($sample_set[$t1]) || $t1 < $t0){  # If T1 doesn't have any pages
+					continue;                               # Skip
+				}
+				foreach($sample_set[$t1] as $t2){           # Loop through each T1->T2
+					if(isset($sample_set[$t0][$t2])){       # If a link shared between T0 and T2 exists
+						if(isset($final[$t0][$t1])){        # If the pages have 1 or more shared links already
+							$final[$t0][$t1]++;             # Increment the shared links by 1
+						}
+						else{                               # If the pages don't already have a shared link
+							$final[$t0][$t1] = 1;           # Specify the pages as having 1 shared link
+						}
+					}
+				}
+			}
+		}
+		$end_time = number_format((microtime(true) - $start_time), 6);
+		echo "<b>Total Time:</b> $end_time seconds<br>";
+		echo '<b>Total Memory Used:</b> '. round((memory_get_peak_usage()/1000000), 2)." MB";
+		#echo "<pre>".print_r($final, true)."</pre>";
+		exit;
+	}
+}
+
+$class = new Run_Loop();
+$class->classConfig($_POST);
+?>
