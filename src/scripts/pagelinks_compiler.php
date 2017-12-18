a3236a4b7834c54aa3561dc9f37285ce1380a366
ch4ot1k <charlesrlemonds@gmail.com>
2017-12-17 22:38:34 -0500

    updated it, 3,027,571 rows

--- src/scripts/pagelinks_compiler.php
+++ src/scripts/pagelinks_compiler.php
@@ -5,15 +5,14 @@ ini_set('memory_limit', '10048M');                              # 10 GB memory l
 $output_file     = "../../pagelinks_final_TSV.txt";
 $start_time     = microtime(TRUE);
 $page_array     = page_array_no_redirects(redirect_array());
-$pagelinks_final = compiled_pagelinks($page_array);
-#1,772,596
-file_put_contents($output_file, print_r($pagelinks_final, TRUE));
+$pagelinks_final_string = implode("\n", compiled_pagelinks($page_array));
+#3,027,571 rows
+file_put_contents($output_file, $pagelinks_final_string);
 $end_time = number_format((microtime(TRUE) - $start_time), 6); # Calculate how long it took
-echo "<hr>Total Time: $end_time (sec.)";                                          # Display the run time results
+echo "Total Time: $end_time (sec.)";                                          # Display the run time results
 
 function redirect_array(){
 	$contents = file_get_contents("../../redirect_parsed.txt", TRUE);  # Convert the redirect_parsed tsv to a string
-	$redirect_array = array();                                                  # Initialize associative array
 	$tok = strtok($contents, "\n");                                       # Split the string by token (new line)
 	while($tok !== FALSE){                                                      # While a token exists, get the next full line
 		$col_a = strtok("\t");                                              # Get the string to the leading tab (Page ID)
@@ -24,7 +23,6 @@ function redirect_array(){
 }
 
 function page_array_no_redirects($redirect_array){
-	$page_array = array();
 	$contents   = file_get_contents("../../page_parsed.txt", TRUE);          # Convert page_parsed tsv to a string
 	$tok        = strtok($contents, "\n");                                             # Split the string by token (new line)
 	while($tok !== FALSE){                                                                   # While a token exists, get the next full line
@@ -41,9 +39,8 @@ function page_array_no_redirects($redirect_array){
 }
 function compiled_pagelinks($page_array){
 	$flipped_page_array = array_flip($page_array);
-	$pagelinks_final = array();
 	$count = 0;
-	$contents        = file_get_contents("../../pagelinks_parsed.txt", TRUE);   # Convert the file to a string
+	$contents = file_get_contents("../../pagelinks_parsed.txt", TRUE);   # Convert the file to a string
 	$tok = strtok($contents, "\n");                                             # Split the string by token (new line)
 	while ($tok !== false) {                                                          # While a token exists, get the next full line
 		$col_a = strtok("\t");                                                    # Get the string to the leading tab (page_id)
@@ -54,6 +51,6 @@ function compiled_pagelinks($page_array){
 			$count++;
 		}
 	}
-	echo 'Count: '.$count;
+	echo 'Count: '.$count."\n";
 	return $pagelinks_final;
 }
