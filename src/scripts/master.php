2306f24334c4c01dbefbf1c108e2dd308be17c49
Steven Holdaway <webpurellc@gmail.com>
2018-01-13 12:34:46 -0500

    creating master-d

--- src/scripts/master.php
+++ src/scripts/master.php
@@ -1,5 +1,60 @@
 <?php
+/**
+ * Notes:
+ *  1) Have to install wget for it to work on Windows
+ *      - https://gist.github.com/evanwill/0207876c3243bbb6863e65ec5dc3f058
+ */
+/* Get the correct directory location regardless of the machine */
+$base_uri = str_replace("\src\scripts\master.php", '', $_SERVER['PHP_SELF']) . "\\";
 
-echo "Starting";
-#shell_exec('src/shell/download-file.sh');
+/* URL Locations */
+define('BASE_URI', $base_uri);
+define('TEMP_FILE', "C:\\Users\\steve\\Dropbox\\tablet\\temp\\");
+
+#echo $base_uri;
+#echo "<pre>".print_r($_SERVER, true)."</pre>";
+
+#echo 'Test';
+#echo "Starting";
+#shell_exec('C:/Users/steve/Dropbox/server/desktop/client_server/datapeak/public_html/wikimap/src/shell/download-file.sh');
+
+
+class Master{
+	public function __construct(){
+	
+	}
+	
+	
+	
+	public function classConfig(){
+		echo "Starting\n";
+		
+		$this->downloadFile();
+		#$temp_file = TEMP_FILE.'\test.txt';
+		
+		#exec('C:/Users/steve/Dropbox/server/desktop/client_server/datapeak/public_html/wikimap/src/shell/download-file.sh 2>&1 &', $output, $return_var);
+		#echo "HERE:\n".$test;
+		#print_r($output);
+	}
+	
+	
+	
+	/**
+	 * Wikipedia Database Dumps: https://dumps.wikimedia.org/enwiki/latest/
+	 */
+	public function downloadFile(){
+		$file_url       = 'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-category.sql.gz';
+		$file_name      = 'test.sql.gz';
+		$temp_directory = TEMP_FILE;
+		
+		
+		$url = 'http://www.het.brown.edu/guide/UNIX-password-security.txt';
+		exec("C:/Users/steve/Dropbox/server/desktop/client_server/datapeak/public_html/wikimap/src/shell/download-file.sh $file_url $file_name $temp_directory", $output, $return_var);
+		print_r($output);
+	}
+}
+
+$class = new Master();
+$class->classConfig();
+
 ?>
--- src/shell/download-file.sh
+++ src/shell/download-file.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "hello world"
+# Get the variables passed from the PHP Script
+download_url=$1
+file_name=$2
+temp_directory=$3
+
+# Change the directory to the temporary file location
+cd $temp_directory
+
+# Download the file
+wget  -O $file_name $download_url
+
+# Unzip the file
+gzip -d $file_name
