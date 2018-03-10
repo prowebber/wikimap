11a86068389e8a1d87345492d6fd99701c294d2f
ch4ot1k <charlesrlemonds@gmail.com>
2018-03-10 16:52:54 -0500

    changed back to plusses

--- src/js/server-bridge.js
+++ src/js/server-bridge.js
@@ -144,13 +144,13 @@ function showWikimapLabels(){
 		var max_z = Math.max.apply(Math, v.z_array);
 		var min_z = Math.min.apply(Math, v.z_array);
 		var z_scale = v.z_array[i]/(max_z - min_z);
-		// var node_label_opacity = 0.8;
-		// var node_font_size = 18;
-		var node_font_size = z_scale*(max_font_size-min_font_size) + min_font_size;
-		var node_label_opacity = z_scale*(max_opacity-min_opacity) + min_opacity;
+		var node_label_opacity = 1;
+		var node_font_size = 20;
+		// var node_font_size = z_scale*(max_font_size-min_font_size) + min_font_size;
+		// var node_label_opacity = z_scale*(max_opacity-min_opacity) + min_opacity;
 		var node_top = v.node_labels[i].x, node_left = v.node_labels[i].y;
-		var node_label = node.name;
-		// var node_label = '+';
+		// var node_label = node.name;
+		var node_label = '+';
 		$('#3d-graph').append("<div class='nodetest' style='opacity: " + node_label_opacity + ";font-size:" + node_font_size + "px;top:" + node_top + "px;left:" + node_left + "px;'>" + node_label + "</div>");
 	});
 }
