51df3afaaaf40997cbb7c9dff42a670b7b9dffee
ch4ot1k <charlesrlemonds@gmail.com>
2018-03-16 14:09:19 -0400

    changed to import from

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './node_modules/3d-force-graph/src/3d-force-graph.js';
-import './lib/globals.js';
-import './lib/server-bridge.js';
+import globals from './lib/globals.js';
+import serverBridge from './lib/server-bridge.js';
 
 export { default } from "./lib/app.js";
