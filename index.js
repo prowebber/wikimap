44667204654f1f1d6095986ea59faeaf52b7ad4c
ch4ot1k <charlesrlemonds@gmail.com>
2018-03-16 13:26:55 -0400

    converted globals.js to export for rollup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import './node_modules/3d-force-graph/src/3d-force-graph.js';
 
 export { default } from "./lib/app.js";
+export { default } from "./lib/globals.js";
+export { default } from "./lib/server-bridge.js";
