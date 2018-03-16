c3cd0025ae0c5f6621130056ccb3860e0fab13bd
ch4ot1k <charlesrlemonds@gmail.com>
2018-03-16 14:03:50 -0400

    fixed the rollup imports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './node_modules/3d-force-graph/src/3d-force-graph.js';
+import './lib/globals.js';
+import './lib/server-bridge.js';
 
 export { default } from "./lib/app.js";
-export { default } from "./lib/globals.js";
-export { default } from "./lib/server-bridge.js";
