5f489b6cdda14ce7f9c26d385430dab216cd2961
ch4ot1k <charlesrlemonds@gmail.com>
2018-03-16 20:32:49 -0400

    closer to working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './node_modules/3d-force-graph/src/3d-force-graph.js';
 import globals from './lib/globals.js';
 import serverBridge from './lib/server-bridge.js';
-
+import $ from 'jquery';
 export { default } from "./lib/app.js";
