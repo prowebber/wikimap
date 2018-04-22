840f5c83d43ef10e3a45c006ed822d11cbab4957
ch4ot1k <charlesrlemonds@gmail.com>
2018-04-21 20:13:13 -0400

    commits but error

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonJs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
 import { name, homepage, version } from './package.json';
 
 export default {
@@ -13,10 +16,16 @@ export default {
       globals: { three: 'THREE' },
       file: `output.js`,
       sourcemap: true,
-      banner: `Wikimap - Shit`
+      banner: `// Version ${name} - Shit`
     }
   ],
   plugins: [
+	  postCss({
+		  plugins: [
+			  postCssSimpleVars(),
+			  postCssNested()
+		  ]
+	  }),
     resolve(),
     commonJs(),
     babel({ exclude: 'node_modules/**' })
