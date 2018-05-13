d0ebfff6433e9beda70e7d59fc790c7ee646fe71
ch4ot1k <charlesrlemonds@gmail.com>
2018-05-13 14:00:42 -0400

    formatting and whitespace

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // import "./src/css/style.css";
-import "./lib/custom.js";
+// import v from "./lib/globals.js";
+// import TrackballControls from 'three-trackballcontrols';
+// v.tbControls = TrackballControls;
+// import "./lib/custom.js";
 export { default } from "./lib/three-forcegraph.js";
